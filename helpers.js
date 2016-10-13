80228286c2a50049ca914a13a6c77322d167ebb1
matthewdias <matthewdias@me.com>
2016-10-13 17:38:49 -0500

    initial commit

---
+++ helpers.js
@@ -0,0 +1,14 @@
+module.exports = {
+  timeToMilliseconds = function(time) {
+    var p = time.split(':')
+    var s = 0
+    var m = 1
+
+    while (p.length > 0) {
+        s += m * parseInt(p.pop(), 10)
+        m *= 60
+    }
+
+    return s * 1000
+  }
+}
---
+++ template.js
@@ -0,0 +1,69 @@
+module.exports = {
+  getTitle = function() {
+
+  }
+
+  getArtist = function() {
+
+  }
+
+  getArtwork = function() {
+
+  }
+
+  getPlaying = function() {
+
+  }
+
+  play = function() {
+
+  }
+
+  pause = function() {
+
+  }
+
+  next = function() {
+
+  }
+
+  previous = function() {
+
+  }
+
+  getLength = function() {
+
+  }
+
+  getPlayhead = function() {
+
+  }
+
+  seek = function(time) {
+
+  }
+
+  getFavType = function() {
+    return ''
+  }
+
+  getFavorite = function() {
+
+  }
+
+  favorite = function() {
+
+  }
+
+  getThumbs = function() {
+
+  }
+
+  thumbsUp = function() {
+
+  }
+
+  thumbsDown = function() {
+
+  }
+}
