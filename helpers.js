2a70638407318c2dc56b2a4d0b47024fab654ce9
matthewdias <matthewdias@me.com>
2016-10-13 18:50:11 -0500

    more module fixes

--- helpers.js
+++ helpers.js
@@ -1,14 +1,12 @@
-module.exports = {
-  timeToMilliseconds = function(time) {
-    var p = time.split(':')
-    var s = 0
-    var m = 1
+exports.timeToMilliseconds = function(time) {
+  var p = time.split(':')
+  var s = 0
+  var m = 1
 
-    while (p.length > 0) {
-        s += m * parseInt(p.pop(), 10)
-        m *= 60
-    }
-
-    return s * 1000
+  while (p.length > 0) {
+      s += m * parseInt(p.pop(), 10)
+      m *= 60
   }
+
+  return s * 1000
 }
