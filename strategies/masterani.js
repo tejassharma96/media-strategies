ee0a5c40dda40d3f0dda60ca46219e64916fe4bd
Tejas Sharma <tejassharma96@users.noreply.github.com>
2017-04-23 09:51:11 +0200

    forgot keywords oops

--- strategies/masterani.js
+++ strategies/masterani.js
@@ -1,23 +1,23 @@
 var playerName = 'Masterani'
 
 var getTitle = function() {
-  details = document.querySelector('.details')
+  var details = document.querySelector('.details')
   return details.querySelector('h1').innerHTML
 }
 
 var getEpisode = function() {
-  details = document.querySelector('.details')
-  episode = details.querySelector('h2').innerHTML.split(' ')
+  var details = document.querySelector('.details')
+  var episode = details.querySelector('h2').innerHTML.split(' ')
   return parseInt(episode[episode.length - 1])
 }
 
 var next = function() {
-  actions = document.querySelector('.actions')
+  var actions = document.querySelector('.actions')
   actions.children[3].click()
 }
 
 var previous = function() {
-  actions = document.querySelector('.actions')
+  var actions = document.querySelector('.actions')
   actions.children[1].click()
 }
 
