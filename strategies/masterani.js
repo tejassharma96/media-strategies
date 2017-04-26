7e1fb17e38d6ec45e75a1119562c15d9f42d114b
matthewdias <matthewdias@me.com>
2017-04-25 20:01:14 -0500

    ocd

--- strategies/masterani.js
+++ strategies/masterani.js
@@ -1,24 +1,20 @@
 var playerName = 'Masterani'
 
 var getTitle = function() {
-  var details = document.querySelector('.details')
-  return details.querySelector('h1').innerHTML
+  return document.querySelector('.details > h1').innerHTML
 }
 
 var getEpisode = function() {
-  var details = document.querySelector('.details')
-  var episode = details.querySelector('h2').innerHTML.split(' ')
+  var episode = document.querySelector('.details > h2').innerHTML.split(' ')
   return parseInt(episode[episode.length - 1])
 }
 
 var next = function() {
-  var actions = document.querySelector('.actions')
-  actions.children[3].click()
+  document.querySelector('.actions').children[3].click()
 }
 
 var previous = function() {
-  var actions = document.querySelector('.actions')
-  actions.children[1].click()
+  document.querySelector('.actions').children[1].click()
 }
 
 module.exports = { playerName, getTitle, getEpisode, next, previous }
