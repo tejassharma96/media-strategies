39128cd24b6c43841a0dd91a82fda528a52b327e
matthewdias <matthewdias@me.com>
2017-04-26 17:15:23 -0500

    Match http and https

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 var mapping = {
-  'http://9anime.to/watch/': '9anime.js',
-  'http://animehaven.to/': 'animehaven.js',
-  'https://twist.moe/a/': 'animetwist.js',
-  'http://www.crunchyroll.com/': 'crunchyroll.js',
-  'http://www.daisuki.net/': 'daisuki.js',
-  'http://www.funimation.com/shows/': 'funimation.js',
-  'https://play.google.com/music/': 'googleplaymusic.js',
-  'http://www.hulu.com/watch/': 'hulu.js',
-  'http://kissanime.ru/Anime/': 'kissanime.js',
-  'http://www.masterani.me/anime/watch': 'masterani.js',
-  'http://moetube.net/watch/': 'moetube.js',
-  'https://www.netflix.com/title/': 'netflix.js',
-  'https://play.pocketcasts.com/': 'pocketcasts.js',
-  'https://soundcloud.com/': 'soundcloud.js',
-  'http://www.viewster.com/serie/': 'viewster.js',
-  'http://www.viz.com/watch/streaming': 'viz.js',
-  'https://www.youtube.com/watch': 'youtube.js'
+  'https?://9anime.to/watch/': '9anime.js',
+  'https?://animehaven.to/': 'animehaven.js',
+  'https?://twist.moe/a/': 'animetwist.js',
+  'https?://www.crunchyroll.com/': 'crunchyroll.js',
+  'https?://www.daisuki.net/': 'daisuki.js',
+  'https?://www.funimation.com/shows/': 'funimation.js',
+  'https?://play.google.com/music/': 'googleplaymusic.js',
+  'https?://www.hulu.com/watch/': 'hulu.js',
+  'https?://kissanime.ru/Anime/': 'kissanime.js',
+  'https?://www.masterani.me/anime/watch': 'masterani.js',
+  'https?://moetube.net/watch/': 'moetube.js',
+  'https?://www.netflix.com/title/': 'netflix.js',
+  'https?://play.pocketcasts.com/': 'pocketcasts.js',
+  'https?://soundcloud.com/': 'soundcloud.js',
+  'https?://www.viewster.com/serie/': 'viewster.js',
+  'https?://www.viz.com/watch/streaming': 'viz.js',
+  'https?://www.youtube.com/watch': 'youtube.js'
 }
 
 var findStrategy = function(url) {
   for (var strategy in mapping) {
-    if (url.indexOf(strategy) != -1) {
+    if (url.match(strategy)) {
       return mapping[strategy]
     }
   }
