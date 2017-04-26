bc112401f483b66294ca5d33706599c77719e20d
matthewdias <matthewdias@me.com>
2017-04-26 17:34:05 -0500

    Add video functions to 9anime

--- strategies/9anime.js
+++ strategies/9anime.js
@@ -8,6 +8,17 @@ var getEpisode = function() {
   return parseInt(document.querySelector('#episode-comment > span').innerHTML)
 }
 
+var getPlaying = function() {
+  return !(document.querySelector('video').paused)
+}
+
+var playPause = function() {
+  var video = document.querySelector('video')
+  if (video.paused)
+    video.play()
+  else video.pause()
+}
+
 var next = function() {
   document.getElementsByClassName('next')[0].click()
 }
@@ -16,4 +27,16 @@ var previous = function() {
   document.getElementsByClassName('prev')[0].click()
 }
 
-module.exports = { playerName, getTitle, getEpisode, next, previous }
+var getLength = function() {
+  return document.querySelector('video').duration * 1000
+}
+
+var getPlayhead = function() {
+  return document.querySelector('video').currentTime * 1000
+}
+
+var seek = function(ms) {
+  document.querySelector('video').currentTime = ms / 1000
+}
+
+module.exports = { playerName, getTitle, getEpisode, getPlaying, playPause, next, previous, getLength, getPlayhead, seek }
