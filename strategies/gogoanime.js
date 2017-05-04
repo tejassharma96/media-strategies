ed57e20fd612ae914cc613381557384d03c2e659
matthewdias <matthewdias@me.com>
2017-05-04 18:20:40 -0500

    Add video tag support back to gogoanime

--- strategies/gogoanime.js
+++ strategies/gogoanime.js
@@ -1,4 +1,4 @@
-var playerName = 'GogoAnime'
+var playerName = 'Gogo'
 
 var getTitle = function() {
   var title = document.querySelector('.title_name > h2').innerHTML
@@ -10,6 +10,17 @@ var getEpisode = function() {
   return parseInt(title.split(' ').slice(-3, -2)[0])
 }
 
+var getPlaying = function() {
+  return !(document.querySelector('video').paused)
+}
+
+var playPause = function() {
+  var video = document.querySelector('video')
+  if (video.paused
+)    video.play()
+  else video.pause()
+}
+
 var next = function() {
   document.querySelector('.anime_video_body_episodes_r > a')
 }
@@ -18,4 +29,16 @@ var previous = function() {
   document.querySelector('.anime_video_body_episodes_l > a')
 }
 
-module.exports = { playerName, getTitle, getEpisode, next, previous }
+var getLength = function() {
+  return document.querySelector('video').duration * 1000
+}
+
+var getPlayhead = function() {
+  return document.querySelector('video').currentTime * 1000
+}
+
+var seek = function(ms) {
+  document.querySelector('video').currentTime = ms / 1000
+}
+
+module.exports = { playerName, getTitle, getEpisode, getPlaying, playPause, next, previous, getLength, getPlayhead, seek }
