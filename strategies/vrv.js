f5b8cbd9e029bee589af162eacf9f9ee2496e537
Tejas Sharma <tejass@zillowgroup.com>
2017-07-03 14:04:43 -0700

    removed video player stuff

--- strategies/vrv.js
+++ strategies/vrv.js
@@ -1,34 +1,11 @@
 var playerName = 'VRV'
 
 var getTitle = function() {
-  return document.querySelector(".series").innerText
+  return document.querySelector('.series').innerText
 }
 
 var getEpisode = function() {
   return parseInt(document.querySelector('.title').innerText.split(' ').slice(1, 2)[0])
 }
 
-var getPlaying = function() {
-  return !(document.querySelector("#player > video").paused)
-}
-
-var playPause = function() {
-  var video = document.querySelector("#player > video")
-  if (video.paused
-)    video.play()
-  else video.pause()
-}
-
-var getLength = function() {
-  return document.querySelector("#player > video").duration * 1000
-}
-
-var getPlayhead = function() {
-  return document.querySelector("#player > video").currentTime * 1000
-}
-
-var seek = function(ms) {
-  document.querySelector("#player > video").currentTime = ms / 1000
-}
-
-module.exports = { playerName, getTitle, getEpisode, getPlaying, playPause, getLength, getPlayhead, seek }
+module.exports = { playerName, getTitle, getEpisode}
